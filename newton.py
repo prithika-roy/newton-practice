33d42cec0e98c5f8667af73a273f1be8538a1f9d
Prithika Roy <prithika_roy@berkeley.edu>
2026-08-19 14:05:47 -0700

    Adding docstrings and reformatting code

--- newton.py
+++ newton.py
@@ -1,10 +1,47 @@
 def derivative(f, x, epsilon=1e-3):
-    return ((f(x + epsilon) - f(x)) / epsilon)
+    """Approximate the first derivative of a function at a point.
+
+    Args:
+        f (callable): Function to differentiate.
+        x (float): Point at which to evaluate the derivative.
+        epsilon (float, optional): Small step size used for finite differences.
+            Defaults to 1e-3.
+
+    Returns:
+        float: Approximate derivative of f at x.
+    """
+    return (f(x + epsilon) - f(x)) / epsilon
+
 
 def second_derivative(f, x, epsilon=1e-3):
+    """Approximate the second derivative of a function at a point.
+
+    Args:
+        f (callable): Function to differentiate twice.
+        x (float): Point at which to evaluate the second derivative.
+        epsilon (float, optional): Small step size used for finite differences.
+            Defaults to 1e-3.
+
+    Returns:
+        float: Approximate second derivative of f at x.
+    """
     return (derivative(f, x + epsilon) - derivative(f, x)) / epsilon
 
+
 def optimize(f, x0, epsilon=1e-3, difference=1e-5):
+    """Use Newton's method to optimize a function starting from an initial guess.
+
+    Args:
+        f (callable): Function to optimize.
+        x0 (float): Initial guess for the optimizer.
+        epsilon (float, optional): Small step size used for finite-difference
+            derivatives. Defaults to 1e-3.
+        difference (float, optional): Convergence threshold based on the change
+            in x between iterations. Defaults to 1e-5.
+
+    Returns:
+        float: Approximate optimizer value after convergence.
+    """
     x = x0
     while True:
         first_der = derivative(f, x, epsilon)
