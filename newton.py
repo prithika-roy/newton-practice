74ae3e2af65ffe35302dfe7b993ad236dce357c1
Brileigh Cates <brileigh_cates@berkeley.edu>
2026-08-20 11:41:43 -0700

    Changed variable H to hes and tolerance to 10e-5.

--- newton.py
+++ newton.py
@@ -2,16 +2,16 @@ import numpy as np
 from scipy.differentiate import jacobian, hessian
 
 
-def newton_multivariate(f, x0, tolerance=1e-4, max_iter=1000):
+def newton_multivariate(f, x0, tolerance=1e-5, max_iter=1000):
     x = np.asarray(x0, dtype=float)
 
     for _ in range(max_iter):
         grad = jacobian(f, x).df
-        H = hessian(f, x).ddf
+        hes = hessian(f, x).ddf
 
         if np.linalg.norm(grad) < tolerance:
             break
 
-        x = x - np.linalg.solve(H, grad)
+        x = x - np.linalg.solve(hes, grad)
 
     return x
